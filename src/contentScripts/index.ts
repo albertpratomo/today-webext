dded80769bede8341e19bfb7ed7aa9a98a729b99
Albert Pratomo <albertpratomo21@gmail.com>
2023-07-18 00:07:59 +0200

    chore: rename common-setup to commonSetup

--- src/contentScripts/index.ts
+++ src/contentScripts/index.ts
@@ -2,7 +2,7 @@
 import {onMessage} from 'webext-bridge/content-script';
 import {createApp} from 'vue';
 import App from './views/App.vue';
-import {setupApp} from '~/logic/common-setup';
+import {setupApp} from '~/logic/commonSetup';
 
 // Firefox `browser.tabs.executeScript()` requires scripts return a primitive value
 (() => {
