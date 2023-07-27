60f33a73f5144e9f6d6a83b99e88d544ac2287c3
Albert Pratomo <albertpratomo21@gmail.com>
2023-07-27 12:15:04 +0200

    test(TaskList): select 1 task

--- src/components/__tests__/TaskList.test.ts
+++ src/components/__tests__/TaskList.test.ts
@@ -1,18 +1,53 @@
-import {render} from '@testing-library/vue';
+import {fireEvent, render} from '@testing-library/vue';
 import {vOnClickOutside} from '@vueuse/components';
 import TaskList from '~/components/TaskList.vue';
 
-describe('TaskList', () => {
-    it('renders tasks', () => {
-        const {getAllByText} = render(TaskList, {
-            global: {
-                directives: {'on-click-outside': vOnClickOutside},
-            },
-            props: {
-                modelValue: [{title: 'task 1'}, {title: 'task 2'}],
-            },
-        });
+function prepare() {
+    // Pass 5 tasks.
+    const modelValue = Array.from({length: 5}, (_, i) => ({title: `task ${i}`}));
 
-        expect(getAllByText(/^task/).length).toBe(2);
+    const result = render(TaskList, {
+        global: {
+            directives: {'on-click-outside': vOnClickOutside},
+        },
+        props: {
+            modelValue,
+        },
     });
+
+    return {
+        ...result,
+        querySelected: () => result.queryByText((_, element) => {
+            return element?.getAttribute('aria-selected') === 'true';
+        }),
+        taskItems: result.getAllByText(/^task/),
+    };
+}
+
+describe('TaskList', () => {
+    test('render tasks', () => {
+        const {taskItems} = prepare();
+
+        expect(taskItems.length).toBe(5);
+    });
+
+    test('select 1 task', async () => {
+        const {taskItems, querySelected} = prepare();
+
+        await fireEvent.click(taskItems[0]);
+        let selected = querySelected();
+        expect(selected?.textContent).toBe('task 0');
+
+        await fireEvent.click(taskItems[2]);
+        selected = querySelected();
+        expect(selected?.textContent).toBe('task 2');
+
+        await fireEvent.click(document);
+        selected = querySelected();
+        expect(selected).toBeNull();
+    });
+
+    test.todo('select 2 tasks');
+    test.todo('select shift 2 tasks');
+    test.todo('select shift 3 task');
 });
