3f711f973170beb26741e4cd88a12c08e4658ce8
Albert Pratomo <albertpratomo21@gmail.com>
2023-12-06 22:43:24 +0100

    refactor(Library): separate into LibraryButtons component

---
+++ src/components/LibraryButtons.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+const buttons = ref([
+    {
+        variant: 'primary',
+    },
+    {
+        variant: 'primary',
+        size: 'sm',
+    },
+    {
+        variant: 'secondary',
+    },
+    {
+        variant: 'critical',
+        size: 'sm',
+    },
+    {
+        variant: 'outline',
+    },
+    {
+        variant: 'outline',
+        size: 'sm',
+    },
+    {
+        variant: 'ghost',
+    },
+    {
+        variant: 'ghost',
+        size: 'sm',
+    },
+]);
+</script>
+
+<template>
+    <ul class="space-y-3">
+        <li
+            v-for="(button, i) in buttons"
+            :key="i"
+            class="flex gap-3"
+        >
+            <Button
+                class="capitalize"
+                :disabled="false"
+                :size="button.size"
+                :variant="button.variant"
+            >
+                {{ button.variant }}
+            </Button>
+
+            <Button
+                class="capitalize"
+                :disabled="true"
+                :size="button.size"
+                :variant="button.variant"
+            >
+                {{ button.variant }}
+            </Button>
+        </li>
+    </ul>
+</template>
