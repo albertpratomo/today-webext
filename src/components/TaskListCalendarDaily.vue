a8f940adad6e30ffee5a1a5151c81af837b69979
Taaniel <taaniel.jakobs@zerply.com>
2023-12-07 13:00:10 +0200

    feat: new component TaskListCalendarDaily

---
+++ src/components/TaskListCalendarDaily.vue
@@ -0,0 +1,94 @@
+<script setup lang="ts">
+import {onKeyStroke} from '~/utils/onKeyStroke';
+import {storeToRefs} from 'pinia';
+import {useDateFormat} from '@vueuse/core';
+import {useTasksStore} from '~/stores';
+
+const props = withDefaults(
+    defineProps<{
+        collection: string
+        date?: Date | null
+    }>(),
+    {
+        date: null,
+    },
+);
+
+const {taskCreateDialogIsOpen} = storeToRefs(useTasksStore());
+
+const tasks = defineModel<Task[]>({required: true});
+const doneTasks = defineModel<Task[]>('doneTasks', {local: true, default: []});
+
+const currentDate = (props.date ? useDateFormat(props.date, 'DD MMM YYYY') : null);
+
+const isCalendarVisible = ref(true);
+
+function toggleCalendar() {
+    isCalendarVisible.value = !isCalendarVisible.value;
+}
+
+onKeyStroke([']'], () => {
+    toggleCalendar();
+});
+</script>
+
+<template>
+    <div class="relative h-full min-w-0 flex">
+        <Button
+            class="absolute right-1 top-1 z-10 text-gray-500"
+            size="square"
+            variant="ghost"
+            @click="toggleCalendar"
+        >
+            <MaterialSymbolsCalendarTodayOutline />
+        </Button>
+
+        <div class="min-w-0 flex grow justify-center overflow-y-auto px-8 py-10 md:px-11">
+            <div class="max-w-[960px] min-w-0 grow">
+                <div class="h-8 flex justify-between">
+                    <h1 class="text-xl font-medium">
+                        {{ $t(`sidebar.${collection}`) }}
+
+                        <span
+                            v-if="currentDate"
+                            class="ml-1 text-gray-500"
+                        >
+                            {{ currentDate }}
+                        </span>
+                    </h1>
+
+                    <Button
+                        id="btn-new-task"
+                        v-tippy="{
+                            content: $t('tooltips.newTask'),
+                            placement: 'bottom',
+                            offset: [0, 6],
+                        }"
+                        class="text-indigo-400"
+                        size="square"
+                        variant="ghost"
+                        @click="taskCreateDialogIsOpen = true"
+                    >
+                        <MaterialSymbolsAdd />
+                    </Button>
+                </div>
+
+                <TaskList
+                    v-model="tasks"
+                    v-model:done-tasks="doneTasks"
+                    class="mt-8 pb-10"
+                />
+            </div>
+        </div>
+
+        <Transition name="slide-left-right">
+            <div
+                v-show="isCalendarVisible"
+                class="shrink-0 border-l border-gray-750 p-2.5"
+                w="70 xl:80"
+            >
+                <CalendarDaily />
+            </div>
+        </Transition>
+    </div>
+</template>
