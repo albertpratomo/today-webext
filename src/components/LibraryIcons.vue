d4052e87915da25a71876063e090ef33abf691ca
Albert Pratomo <albertpratomo21@gmail.com>
2023-12-29 20:28:34 +0100

    refactor: make LibraryIcons component

---
+++ src/components/LibraryIcons.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const icons = [
+    {
+        name: 'flower',
+    },
+    {
+        name: 'today',
+    },
+    {
+        name: 'later',
+    },
+    {
+        name: 'trash',
+    },
+];
+</script>
+
+<template>
+    <ul class="flex space-x-4">
+        <li
+            v-for="(icon, i) in icons"
+            :key="i"
+        >
+            <Icon
+                class="mr-1.5 h-4 w-4"
+                :name="icon.name"
+            />
+        </li>
+    </ul>
+</template>
--- src/options/pages/Library.vue
+++ src/options/pages/Library.vue
@@ -1,38 +1,7 @@
-<script setup lang="ts">
-const icons = [
-    {
-        id: 1,
-        name: 'flower',
-    },
-    {
-        id: 2,
-        name: 'today',
-    },
-    {
-        id: 3,
-        name: 'later',
-    },
-    {
-        id: 4,
-        name: 'trash',
-    },
-];
-</script>
-
 <template>
-    <div class="p-10">
+    <div class="p-10 space-y-8">
         <LibraryButtons />
 
-        <ul>
-            <li
-                v-for="(icon, i) in icons"
-                :key="i"
-            >
-                <Icon
-                    class="mr-1.5 h-[16px] w-[16px]"
-                    :name="icon.name"
-                />
-            </li>
-        </ul>
+        <LibraryIcons />
     </div>
 </template>
